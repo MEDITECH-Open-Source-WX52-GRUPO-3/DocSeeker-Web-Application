1cafd26d7fc3c47e3a375a846a86d4ef8cb66abf
Leonardo Cesías Díaz <u202010773@upc.edu.pe>
2023-04-28 12:09:26 -0500

    Added dash button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,29 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import { LogInComponent } from './views/patients/log-in/log-in.component';
+import { DashButtonComponent } from './dash-button/dash-button.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {NgOptimizedImage} from "@angular/common";
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ToolbarComponent,
-    LogInComponent
+    LogInComponent,
+    DashButtonComponent
   ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        MatToolbarModule
+        MatToolbarModule,
+        MatButtonModule,
+        MatIconModule,
+        NgOptimizedImage,
+        MatCardModule
     ],
   providers: [],
   bootstrap: [AppComponent]
