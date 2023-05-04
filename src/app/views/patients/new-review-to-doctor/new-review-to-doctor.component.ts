a48f8f8efc51f5425aa9c2c377e640ac0a40d1ed
Leonardo Cesías Díaz <u202010773@upc.edu.pe>
2023-05-03 22:25:27 -0500

    Added first version of review to doctor component

--- src/app/views/patients/new-review-to-doctor/new-review-to-doctor.component.ts
+++ src/app/views/patients/new-review-to-doctor/new-review-to-doctor.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {map, Observable, shareReplay} from "rxjs";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
+import {ActivatedRoute, Router} from "@angular/router";
+import {SourcesService} from "../../../services/sources.service";
 
 @Component({
   selector: 'app-new-review-to-doctor',
   templateUrl: './new-review-to-doctor.component.html',
   styleUrls: ['./new-review-to-doctor.component.css']
 })
-export class NewReviewToDoctorComponent {
+export class NewReviewToDoctorComponent implements OnInit{
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+  doctors: Array<any> = [];
+  doctor: any;
+  id="" ;
 
+  constructor(private route: ActivatedRoute, private breakpointObserver: BreakpointObserver, private newsSource: SourcesService, private router: Router) {}
+
+  ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+    this.newsSource.getSources('doctors').subscribe((data: any): void => {
+      this.doctors = data;
+      this.doctor = this.doctors.find(doctor => doctor.id == this.id);
+
+      console.log("Sources: ", this.doctor);
+      console.log("Sources: ", this.id);
+
+    });
+  }
 }
