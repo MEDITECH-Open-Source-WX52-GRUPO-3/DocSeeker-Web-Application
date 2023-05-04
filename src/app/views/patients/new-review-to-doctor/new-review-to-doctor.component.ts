ba4b76a287fb2267e41e7bcd569a65061c644d7b
Leonardo Cesías Díaz <u202010773@upc.edu.pe>
2023-05-04 13:57:16 -0500

    Added first version of rating using ignite ui

--- src/app/views/patients/new-review-to-doctor/new-review-to-doctor.component.ts
+++ src/app/views/patients/new-review-to-doctor/new-review-to-doctor.component.ts
@@ -3,11 +3,19 @@ import {map, Observable, shareReplay} from "rxjs";
 import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
 import {ActivatedRoute, Router} from "@angular/router";
 import {SourcesService} from "../../../services/sources.service";
+import { defineComponents, IgcRatingComponent } from 'igniteui-webcomponents';
 
+defineComponents(IgcRatingComponent);
+export class RatingBasic {
+  constructor() {
+  }
+}
+
+new RatingBasic();
 @Component({
   selector: 'app-new-review-to-doctor',
   templateUrl: './new-review-to-doctor.component.html',
-  styleUrls: ['./new-review-to-doctor.component.css']
+  styleUrls: ['./new-review-to-doctor.component.css'],
 })
 export class NewReviewToDoctorComponent implements OnInit{
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
@@ -19,6 +27,7 @@ export class NewReviewToDoctorComponent implements OnInit{
   doctor: any;
   id="" ;
   review: any;
+  selectedValue: any;
 
   constructor(private route: ActivatedRoute, private breakpointObserver: BreakpointObserver, private newsSource: SourcesService, private router: Router) {}
 
@@ -32,5 +41,9 @@ export class NewReviewToDoctorComponent implements OnInit{
       console.log("Sources: ", this.id);
 
     });
+  }
+
+  ratingChanged($event: any) {
+
   }
 }
