48f6595f2a6cd8cc7cb8de64b7cd487619c3f3aa
Leonardo Cesías Díaz <u202010773@upc.edu.pe>
2023-05-27 14:37:13 -0500

    Added first version of prescriptions view

--- src/app/views/patients/prescriptions/prescriptions.component.ts
+++ src/app/views/patients/prescriptions/prescriptions.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {map, Observable, shareReplay} from "rxjs";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
+import {ActivatedRoute, Router} from "@angular/router";
+import {SourcesService} from "../../../services/sources.service";
 
 @Component({
   selector: 'app-prescriptions',
   templateUrl: './prescriptions.component.html',
   styleUrls: ['./prescriptions.component.css']
 })
-export class PrescriptionsComponent {
+export class PrescriptionsComponent implements OnInit {
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+  prescriptions: Array<any> = [];
+  prescription: any;
+  id = "";
+  constructor(private route: ActivatedRoute, private breakpointObserver: BreakpointObserver, private prescriptionsSource: SourcesService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.prescriptionsSource.getSources('prescriptions').subscribe((data: any): void =>{
+      this.prescriptions = data;
+      this.prescription = this.prescriptions.find(prescription => prescription.id == this.id);
+
+      console.log("Sources: ", this.prescription);
+      console.log("Sources: ", this.id);
+    });
+  }
+
+
 
 }
