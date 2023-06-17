c68eb9c06748992fc2a7adfb4b8e83139dd6569b
Leonardo Cesías Díaz <u202010773@upc.edu.pe>
2023-06-17 16:14:07 -0500

    Updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,6 +21,7 @@ import {MedicalHistoryListComponent} from "./views/doctors/medical-history-list/
 import {MedicalHistoryPatientComponent} from "./views/doctors/medical-history-patient/medical-history-patient.component";
 import {AppointmentsComponent} from "./views/doctors/appointments/appointments.component";
 import {DashboardDoctorComponent} from "./views/doctors/dashboard-doctor/dashboard-doctor.component";
+import {NewMedicalHistoryComponent} from "./views/doctors/new-medical-history/new-medical-history.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -45,7 +46,7 @@ const routes: Routes = [
   { path: 'listPatientsMedicalHistory/:idPatient', component: MedicalHistoryPatientComponent },
   { path: 'myAppointments', component: AppointmentsComponent },
   { path: 'dashboardDoctor', component: DashboardDoctorComponent },
-
+  { path: 'newMedicalHistory', component: NewMedicalHistoryComponent}
 
 
 
